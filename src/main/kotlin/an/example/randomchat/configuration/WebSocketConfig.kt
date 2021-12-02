5a279d24cf01c81c18299a0c7b899e973f2a47c8
Skipancho <ctg1770a@gmail.com>
2021-12-02 20:20:05 +0900

    + @EnableWebSocket 애노테이션 추가

--- src/main/kotlin/an/example/randomchat/configuration/WebSocketConfig.kt
+++ src/main/kotlin/an/example/randomchat/configuration/WebSocketConfig.kt
@@ -2,11 +2,13 @@ package an.example.randomchat.configuration
 
 import an.example.randomchat.domain.chat.websocket.WSRandomChatHandler
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 
 @Configuration
+@EnableWebSocket
 class WebSocketConfig(
     private val wsRandomChatHandler: WSRandomChatHandler
 ) : WebSocketConfigurer{
